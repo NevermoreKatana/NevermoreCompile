5759908ee519fed4130c0605e59d231f7845f2da
NevermoreKatana <grenkachannel@hotmail.com>
2024-03-19 00:11:18 +0300

    update homepage

--- WebCompiler/settings.py
+++ WebCompiler/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'WebCompiler'
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'WebCompiler/static/'
+
+# if not DEBUG:
+#     STATIC_ROOT = 'souls_of_stockholm/staticfiles'
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
