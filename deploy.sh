03c82b5de4d5c825c778dfabe3fe868d197374c1
NevermoreKatana <grenkachannel@hotmail.com>
2024-03-19 18:33:23 +0300

    fix brain

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-apt-get update
 
 apt-get install -y clang
-apt-get install python3-pip
-pip install -r requirements.txt
+
 echo 'export PATH=/usr/bin:$PATH' >> ~/.bashrc
 
 source ~/.bashrc
-python3 manage.py collectstatic --no-input
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
