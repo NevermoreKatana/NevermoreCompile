3cf5743fa813fbe8d77290b97bf0efbfc7e3bd5d
NevermoreKatana <grenkachannel@hotmail.com>
2024-03-19 18:38:05 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-
-apt-get install -y clang
-
-echo 'export PATH=/usr/bin:$PATH' >> ~/.bashrc
-
-source ~/.bashrc
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
