023d39325ea977d6cb11afc05b82454c0f82ee66
NevermoreKatana <grenkachannel@hotmail.com>
2024-03-22 12:06:10 +0300

    add my optimization

--- compil/optimizer.py
+++ compil/optimizer.py
@@ -8,7 +8,7 @@ sys.path.insert(0, current_directory)
 from llvmlite import ir, binding
 from compil.mixins import ReadWriteMixin
 import re
-pattern = r'(add|sub|mul|udiv|fdiv|fadd|fmul|fsub) (i32|double) (0x[0-9a-fA-F]+|\d+), (0x[0-9a-fA-F]+|\d+)'
+pattern = r' (add|sub|mul|udiv|fdiv|fadd|fmul|fsub) (i32|double) (0x[0-9a-fA-F]+|\d+), (0x[0-9a-fA-F]+|\d+)'
 class Optimizer(ReadWriteMixin):
     def __init__(self, filename: str) -> None:
         self.ll_file = filename
@@ -49,20 +49,18 @@ class Optimizer(ReadWriteMixin):
         pass_manager.run(self.module)
 
 
-class MyOptimizer(ReadWriteMixin):
-        
-    def __init__(self, filename) -> None:
+class NeplOptimizer:
+    def __init__(self, input_file) -> None:
         self.module = ir.Module("nevermoreCompile")
         self.module.triple = 'x86_64-pc-linux-gnu'
-        self.variables = {}
-        self.functions = {}
-        self.ll_file = filename
-        self.ll_reader()
-        self.optimized_file = None
-        self.init_opt_file()
+        self.ll_file = self.reader(input_file)
+        self.builder_init()
         
-    def init_opt_file(self):
-       
+    def reader(self, filename):
+        with open(filename, 'r') as f:
+            return f.read()
+    
+    def builder_init(self) -> None:
         func_name = 'main'
         func_type = ir.VoidType()
 
@@ -72,8 +70,7 @@ class MyOptimizer(ReadWriteMixin):
         block = main_func.append_basic_block(name=f"entry.{func_name}")
         builder = ir.IRBuilder(block)
         self.builder = builder
-        self.functions[func_name] = builder
-        
+    
     def hex_from_str(self, num1, num2):
         if num1.startswith('0x'):
             num1 = num1[2:]
@@ -87,69 +84,121 @@ class MyOptimizer(ReadWriteMixin):
         num2 = struct.unpack('!d', num2)[0]
         
         return num1, num2
+    
+    def add(self, line):
+        if self.data_type == 'i32':
+            result = int(int(self.num1) + int(self.num2))
+            
+            line1 = f'  %"{self.var_name}" = alloca i32'
+            line2 = f'  store i32 {result}, i32* %"{self.var_name}"'
+            line3 = '' if 'store' in line else line
+            return line1, line2, line3
+
+        if self.data_type == 'double':
+            num1, num2 = self.hex_from_str(self.num1, self.num2)
+            result = float(float(num1) + float(num2))
+            
+            line1 = f'  %"{self.var_name}" = alloca double'
+            line2 = f'  store double {result}, double* %"{self.var_name}"'
+            line3 = '' if 'store' in line else line
+            return line1, line2, line3
+        
+    def sub(self,line):
+        if self.data_type == 'i32':
+            result = int(int(self.num1) - int(self.num2))
+            
+            line1 = f'  %"{self.var_name}" = alloca i32'
+            line2 = f'  store i32 {result}, i32* %"{self.var_name}"'
+            line3 = '' if 'store' in line else line
+            return line1, line2, line3
+
+        if self.data_type == 'double':
+            num1, num2 = self.hex_from_str(self.num1, self.num2)
+            result = float(float(num1) - float(num2))
+            
+            line1 = f'  %"{self.var_name}" = alloca double'
+            line2 = f'  store double {result}, double* %"{self.var_name}"'
+            line3 = '' if 'store' in line else line
+            return line1, line2, line3
+        
+    def div(self,line):
+        if self.data_type == 'i32':
+            result = int(int(self.num1) / int(self.num2))
+            
+            line1 = f'  %"{self.var_name}" = alloca i32'
+            line2 = f'  store i32 {result}, i32* %"{self.var_name}"'
+            line3 = '' if 'store' in line else line
+            return line1, line2, line3
+
+        if self.data_type == 'double':
+            num1, num2 = self.hex_from_str(self.num1, self.num2)
+            result = float(float(num1) / float(num2))
+            
+            line1 = f'  %"{self.var_name}" = alloca double'
+            line2 = f'  store double {result}, double* %"{self.var_name}"'
+            line3 = '' if 'store' in line else line
+            return line1, line2, line3
+    
+    def mul(self, line):
+        if self.data_type == 'i32':
+            result = int(int(self.num1) * int(self.num2))
+            
+            line1 = f'  %"{self.var_name}" = alloca i32'
+            line2 = f'  store i32 {result}, i32* %"{self.var_name}"'
+            line3 = '' if 'store' in line else line
+            return line1, line2, line3
+
+        if self.data_type == 'double':
+            num1, num2 = self.hex_from_str(self.num1, self.num2)
+            result = float(float(num1) * float(num2))
+            
+            line1 = f'  %"{self.var_name}" = alloca double'
+            line2 = f'  store double {result}, double* %"{self.var_name}"'
+            line3 = '' if 'store' in line else line
+            return line1, line2, line3
+        
+        
     def math_operands(self):
         lines = self.ll_file.split('\n')
+        self.ll_file = ''
         for i in range(len(lines)):
             line = lines[i]
-            matches = re.findall(pattern, line)
+            matches = re.finditer(pattern, line)
 
-            if matches:
-                for match in matches:
-                    operation, data_type, num1, num2 = match
-                    var_name = lines[i+1]
-                    var_name = re.search(r'"([^"]*)"', var_name).group(1)
-                    if operation in ['add', 'fadd']:
-                        if data_type == 'i32':
-                            result = int(int(num1) + int(num2))
-                            
-                            lines[i] = f'\t%"{var_name}" = alloca i32'
-                            lines[i+1] = f'\tstore i32 {result}, i32* %"{var_name}"'
-                            lines[i+2] = ''
+            for match in matches:
+                operation, self.data_type, self.num1, self.num2 = match.groups()
+                self.var_name = lines[i+1]
+                self.var_name = re.search(r'"([a-zA-Zа-яА-Я][a-zA-Zа-яА-Я0-9]*)"', self.var_name).group(1) if re.search(r'"([a-zA-Zа-яА-Я][a-zA-Zа-яА-Я0-9]*)"', self.var_name) else None
 
-                        if data_type == 'double':
-                            num1, num2 = self.hex_from_str(num1, num2)
-                            result = float(float(num1) + float(num2))
-                            
-                            lines[i] = f'\t%"{var_name}" = alloca double'
-                            lines[i+1] = f'store double {result}, double* %"{var_name}"'
-                            lines[i+2] = ''
-
-                    elif operation in ['sub', 'fsub']:
-                        if data_type == 'i32':
-                            result = int(int(num1) - int(num2))
-                            lines[i] = f'\t%"{var_name}" = alloca i32'
-                            lines[i+1] = f'\tstore i32 {result}, i32* %"{var_name}"'
-                            lines[i+2] = ''
-
-                        if data_type == 'double':
-                            num1, num2 = self.hex_from_str(num1, num2)
-                            result = float(float(num1) - float(num2))
-                            lines[i] = f'\t%"{var_name}" = alloca double'
-                            lines[i+1] = f'\tstore double {result}, double* %"{var_name}"'
-                            lines[i+2] =''
+                
+                if operation in ['add', 'fadd'] and self.var_name is not None:
+                    lines[i],lines[i+1], lines[i+2]  = self.add(lines[i+2])
+                if operation in ['sub', 'fsub']and self.var_name is not None:
+                    lines[i],lines[i+1], lines[i+2] = self.sub(lines[i+2])
+                if operation in ['mul', 'fmul']and self.var_name :
+                    lines[i],lines[i+1], lines[i+2] = self.mul(lines[i+2])
 
         self.ll_file = '\n'.join(lines)
+
     
     def write(self, filename):
         with open(filename, 'w') as f:
             f.write(self.ll_file)
 
-
+    def __str__(self) -> str:
+        return str(self.ll_file)
 def optimize_ll(ll_output_file: str = None, ll_optimize_file: str = None) -> None:
-    # optimzer1 = MyOptimizer(ll_output_file)
-    # optimzer1.math_operands()
-    # optimzer1.write(ll_optimize_file)
-    optimzer = Optimizer(ll_output_file)
-    optimzer.optimize()
-    optimzer.ll_writer(ll_optimize_file)
+    try:
+        print("First optimization\nDeleting math operations")
+        optimzer1 = NeplOptimizer(ll_output_file)
+        optimzer1.math_operands()
+        optimzer1.write(ll_optimize_file)
+        print("Second optimization")
+        optimzer = Optimizer(ll_optimize_file)
+        optimzer.optimize()
+        optimzer.ll_writer(ll_optimize_file)
+    except:
+        optimzer = Optimizer(ll_output_file)
+        optimzer.optimize()
+        optimzer.ll_writer(ll_optimize_file)
 
-
-
-
-
-
-
-
-# opt = MyOptimizer('compil/outpit_files/output.ll')
-
-# opt.math_operands()
