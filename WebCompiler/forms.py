6b86c0784ff58fc4d745482cca62d7d8f321bdd2
NevermoreKatana <grenkachannel@hotmail.com>
2024-03-29 12:50:49 +0300

    Добавлено начальное значение

--- WebCompiler/forms.py
+++ WebCompiler/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 
+init = 'void main(){\n\n\n};'
+
 class CompilerForm(forms.Form):
-    input_code = forms.CharField(label='Code', widget=forms.Textarea(attrs={
+    input_code = forms.CharField(label='Code',initial=init, widget=forms.Textarea(attrs={
         'class': 'flex rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 min-h-[470px] text-sm font-mono w-full resize-none', 'placeholder':'Enter your code here','id':'code'}))
