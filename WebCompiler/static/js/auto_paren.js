f7abb6671d378268a0cd4a9e74600f2c1aad5747
NevermoreKatana <grenkachannel@hotmail.com>
2024-03-29 13:15:01 +0300

    Добавлен функционал автоматического дополнения скобок

---
+++ WebCompiler/static/js/auto_paren.js
@@ -0,0 +1,23 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === '{') {
+        event.preventDefault();
+        var editor = document.activeElement;
+        var start = editor.selectionStart;
+        var end = editor.selectionEnd;
+        var text = editor.value;
+        editor.value = text.substring(0, start) + '{\n\n};' + text.substring(end);
+        editor.selectionStart = editor.selectionEnd = start + 2;
+    }
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === '(') {
+        event.preventDefault();
+        var editor = document.activeElement;
+        var start = editor.selectionStart;
+        var end = editor.selectionEnd;
+        var text = editor.value;
+        editor.value = text.substring(0, start) + '()' + text.substring(end);
+        editor.selectionStart = editor.selectionEnd = start + 1;
+    }
+});
--- tmp_files/tmprzz89clr.ll
+++
@@ -1,8 +0,0 @@
-; ModuleID = '<string>'
-source_filename = "<string>"
-target triple = "x86_64-pc-linux-gnu"
-
-define void @main() local_unnamed_addr {
-entry.main:
-  ret void
-}
